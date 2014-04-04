2c26dd401558a88a4bae7d85abd112be6f73ed90
Etienne Millon <me@emillon.org>
2014-04-04 13:51:47 +0200

    input

--- gallery.py
+++ gallery.py
@@ -1,5 +1,35 @@
+import os.path
+import sys
+from mako.template import Template
+
+
+class Example(object):
+
+    def __init__(self, scade, xml, b):
+        self.scade = scade
+        self.xml = xml
+        self.b = b
+
+    @staticmethod
+    def from_test(path):
+        with open(os.path.join(path, 'KCG/kcg_xml_filter_out.scade')) as f:
+            scade = f.read()
+        with open(os.path.join(path, 'KCG/kcg_trace.xml')) as f:
+            xml = f.read()
+        b = "B"
+        return Example(scade, xml, b)
+
+    def render(self):
+        tpl_file = 'templates/gallery.mako'
+        tpl = Template(filename=tpl_file, default_filters=['h'])
+        out = tpl.render(scade=self.scade, xml=self.xml, b=self.b)
+        return out.encode('utf8')
+
+
 def main():
-    pass
+    test_path = sys.argv[1]
+    e = Example.from_test(test_path)
+    print e.render()
 
 if __name__ == '__main__':
     main()
---
+++ templates/gallery.mako
@@ -0,0 +1,21 @@
+<html>
+<head>
+<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+<h3>Scade</h3>
+<pre>
+${scade}
+</pre>
+<h3>XML</h3>
+<pre>
+${xml}
+</pre>
+<h3>B</h3>
+<pre>
+${b}
+</pre>
+</div>
+</body>
+</html>
